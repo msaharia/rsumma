5e677a49a8c305d5284ac1e77fad90af050f2dfe
msaharia <manab@ucar.edu>
2017-09-22 18:41:54 -0600

    Config and Run Scripts for SUMMA parallelization

--- R/create_job_list.R
+++ R/create_job_list.R
@@ -23,30 +23,6 @@ summa <- function(config, nGRU){
 
 }
 
-
-#PLAY
-
-runStart <- lubridate::ymd_hm('2016-10-01 00:00') #Start of the entire run
-st <- runStart + months(0:2)
-et <- runStart + months(1:3) - lubridate::dhours(1)
-mult_one<-function(st,et, num_of_files){
-  readLines("data_raw/PNW2/summa_zDecisions_template.txt") %>%
-    gsub(pattern = "starttime", replace = st) %>%
-    gsub(pattern = "endtime", replace = et) %>%
-    # write.table('data_raw/PNW2/summazd.txt', row.names = F, col.names = F, quote = F)
-    write.table(.,paste("data_raw/PNW2/template_", num_of_files, ".txt",sep=""),
-                row.names=F, col.names=F, quote = F)
-}
-x <- mapply(mult_one,st,et,1:3)
-
-# write.table(x[,3],'data_raw/PNW2/summazd.txt', row.names = F, col.names = F)
-
-paste0('summa_zDecisions_', format(st, format="%Y%m%d"), '_',format(et, format="%Y%m%d"), ".txt")
-
-
-
-
-
 # # s <<- sapply(grustart, function(i) paste(summaexe, "g", i, nGRU, "-m", config))
 # a <-summa('/d3/msaharia/SHARP/summa/PNW2/summa_fileManager_PNW.txt', 20)
 # system(paste(a[1]))
--- R/create_zDecisions_files.R
+++ R/create_zDecisions_files.R
@@ -1,29 +1,40 @@
 #' Create zDecision files for every month automatically
 #'
 #' @param zDecFile zDecisions template file
-#' @param startDate Start date of the simulation
-#' @param nMonths Number of months from the start date for which files are to be created
 #'
 #' @examples
-#' zDecs('data_raw/PNW2/summa_zDecisions_template.txt', '2016-10-01 00:00', 20)
-zDecs <- function(zDecFile, startDate, nMonths){
+#' zDecNames <- zDecs('data_raw/PNW2/summa_zDecisions_template.txt')
+zDecs <- function(zDecFile){
   runStart <- lubridate::ymd_hm(startDate) #Start date of the entire simulation
-  startTimes <- runStart + months(0:(nMonths-1)) #The start time of every simulation batch
-  endTimes <- runStart + months(1:nMonths) - lubridate::dhours(1) #The end time of every simulation batch
-  zDecNames <<- paste0('summa_zDecisions_', format(startTimes, format="%Y%m%d"), '_',
+  startTimes <<- runStart + months(0:(nMonths-1)) #The start time of every simulation batch
+  endTimes <<- runStart + months(1:nMonths) - lubridate::dhours(1) #The end time of every simulation batch
+  zDecNames <- paste0('summa_zDecisions_', format(startTimes, format="%Y%m%d"), '_',
                        format(endTimes, format="%Y%m%d"), ".txt") #zDecision file names
 
   #Meta-Function for creating the separate zDecisions files
   multx<-function(startTimes, endTimes, zDecNames){
     readLines(zDecFile) %>%
-      gsub(pattern = "starttime", replace = startTimes) %>% #Replaces the start time
-      gsub(pattern = "endtime", replace = endTimes) %>% #Replaces the end time
-      write.table(., paste0("zDecisionFiles/", zDecNames), #Writes out a decision file for every batch
+      gsub(pattern = "starttime", replace = format(startTimes, "%F %T")) %>% #Replaces the start time
+      gsub(pattern = "endtime", replace = format(endTimes, "%F %T")) %>% #Replaces the end time
+      write.table(., paste0("data_raw/PNW2/zDecisionFiles/", zDecNames), #Writes out a decision file for every batch
                   row.names=F, col.names=F, quote = F)
   }
   x <- mapply(multx, startTimes, endTimes, zDecNames)
-
+  #pro <- list(startTimes, endTimes, zDecNames)
+  return(zDecNames)
 }
 
+template <- readLines('data_raw/PNW2/summa_zDecisions_template.txt')
+file_data <- Map(function(startTimes, endTimes, name){
+  filled_template <- gsub("starttime", format(startTimes, "%F %T"), template, fixed = TRUE)
+  filled_template <- gsub("endtime", format(endTimes, "%F %T"), filled_template, fixed = TRUE)
+  writeLines(filled_template, file.path("data_raw/PNW2/zDecisionFiles/", name))
+  filled_template    # return vector written to file
+},
+start = startTimes,
+end = endTimes,
+name = paste0('summa_zDecisions_', format(startTimes, format="%Y%m%d"), '_',
+              format(endTimes, format="%Y%m%d"), ".txt") #zDecision file names
+)
 
 
