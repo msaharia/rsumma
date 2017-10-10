abacff14745ae777ad9da0024287307c77f46557
msaharia <manab@ucar.edu>
2017-10-10 14:08:32 -0600

    Temporal plot with GRU index

---
+++ R/0_output_temporal_singleGRU_data_WITHHRUINDEX.R
@@ -0,0 +1,188 @@
+#' Returns a dataframe of SUMMA output variables for plotting purposes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' temporalplot('data_raw/benchmarking/PNW_3L_1H.nc', 1, 'figures/summaoutput_3L_1H.pdf')
+temporalplot <- function(ncname, hruIndex, outfile){
+
+  ########################################################################
+  # Opens the NetCDF file
+  ########################################################################
+  # ncname <- file.path(ncname,fsep = .Platform$file.sep) #Get full file path of the NetCDF file
+  summaout <- nc_open(ncname) #Open the SUMMA output NetCDF file
+  ncvars <- names(summaout[['var']]) #Get all the variables in the netCDF file
+
+  ########################################################################
+  # Functions to extract a particular variable and the time indices
+  ########################################################################
+  stime <- ncvar_get(summaout, 'time') #seconds since 1990
+  stime <- as.POSIXct(stime, origin="1990-01-01 00:00", tz = "GMT")
+
+  #Forcings
+  pptrate <- ncvar_get(summaout, 'pptrate')[hruIndex,]*3600 #Convert from kgm-2s-1 to mm/hour
+  airtemp <- ncvar_get(summaout, 'airtemp')[hruIndex,]-272.15 #Convert to Centigrade
+  windspd <- ncvar_get(summaout, 'windspd')[hruIndex,] #m/s
+  scalarRainfall <- ncvar_get(summaout, 'scalarRainfall')[hruIndex,]*3600 # "computed rainfall rate (instant)" # Convert from kgm-2s-1 to mm/hour
+  SWRadAtm <- ncvar_get(summaout, 'SWRadAtm')[hruIndex,]
+  LWRadAtm <- ncvar_get(summaout, 'LWRadAtm')[hruIndex,]
+
+  #Others
+  scalarSurfaceTemp <- ncvar_get(summaout, 'scalarSurfaceTemp')[hruIndex,] -272.15 #Convert to Centigrade
+
+  #Canopy
+  scalarCanopyIce <- ncvar_get(summaout, 'scalarCanopyIce')[hruIndex,] #mass of ice on the vegetation canopy (instant), kg m-2
+  scalarCanopyLiq <- ncvar_get(summaout, 'scalarCanopyLiq')[hruIndex,]
+  scalarSnowDepth <- ncvar_get(summaout, 'scalarSnowDepth')[hruIndex,]*1000 #From m to mm
+  scalarBelowCanopySolar <- ncvar_get(summaout, 'scalarBelowCanopySolar')[hruIndex,]
+  scalarWindspdCanopyBottom <- ncvar_get(summaout, 'scalarWindspdCanopyBottom')[hruIndex,]*3600000 #m/s to mm/h
+  scalarSenHeatGround <- ncvar_get(summaout, 'scalarSenHeatGround')[hruIndex,]
+  scalarSenHeatTotal <- ncvar_get(summaout, 'scalarSenHeatTotal')[hruIndex,]
+  scalarLatHeatGround <- ncvar_get(summaout, 'scalarLatHeatGround')[hruIndex,]
+  scalarLatHeatTotal <- ncvar_get(summaout, 'scalarLatHeatTotal')[hruIndex,]
+
+  #Soil
+  scalarSnowSublimation <- ncvar_get(summaout, 'scalarSnowSublimation')[hruIndex,]*3600 #Convert from kgm-2s-1 to mm/hour
+  scalarThroughfallSnow <- ncvar_get(summaout, 'scalarThroughfallSnow')[hruIndex,]*3600 #Convert from kgm-2s-1 to mm/hour
+  scalarThroughfallRain <- ncvar_get(summaout, 'scalarThroughfallRain')[hruIndex,]*3600 #Convert from kgm-2s-1 to mm/hour
+  scalarRainPlusMelt <- ncvar_get(summaout, 'scalarRainPlusMelt')[hruIndex,]*3600000 #m/s to mm/h
+  scalarInfiltration <- ncvar_get(summaout, 'scalarInfiltration')[hruIndex,]*3600000 #m/s to mm/h
+  scalarExfiltration <- ncvar_get(summaout, 'scalarExfiltration')[hruIndex,]*3600000 #m/s to mm/h
+  scalarSWE <- ncvar_get(summaout, 'scalarSWE')[hruIndex,]*3600000 #m/s to mm/h
+  scalarSurfaceRunoff <- ncvar_get(summaout, 'scalarSurfaceRunoff')[hruIndex,]*3600000 #m/s to mm/h
+  scalarSoilDrainage <- ncvar_get(summaout, 'scalarSoilDrainage')[hruIndex,]*3600000 #m/s to mm/h
+
+  # Layer information
+  nLayers <- ncvar_get(summaout, 'nLayers')[hruIndex,] #Total of layers at every time step
+  nSnow <- ncvar_get(summaout, 'nSnow')[hruIndex,] #Number of snow layers at every time step
+  nSoil <- ncvar_get(summaout, 'nSoil')[hruIndex,] #Number of soil layers at every time step
+  nLayers <- ncvar_get(summaout, 'nLayers')[hruIndex,] #Number of snow layers at every time step
+
+  ########################################################################
+  # Soil Moisture
+  ########################################################################
+  #Dimensions
+  midTotoStartIndex <- ncvar_get(summaout, 'midTotoStartIndex')[hruIndex,] #start index of the midToto (snow+soil) vector for a given timestep
+
+  #Make a list of soil layer indices ignoring the snow layers
+  soillayerindexlist <- lapply(seq(1,max(nSoil)), function(x) midTotoStartIndex+nSnow+x-1)
+
+  #Extracting for soil moisture for different layers where total no. of layers is max(nLayers)
+  mLayerVolFracLiq <- ncvar_get(summaout, 'mLayerVolFracLiq')[hruIndex,] #Soil Moisture
+
+  #Separate the soil moisture for different soil layers for easier plotting
+  mLayerVolFracLiq_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerVolFracLiq[x])) #Make a list of data
+  mLayerVolFracLiq_soil <- setNames(mLayerVolFracLiq_soil,  #Set the names of different soil layers automatically
+           lapply(seq(1,max(nSoil)), function(x) paste0('mLayerVolFracLiq_SoilLayer',x)))
+
+  ########################################################################
+  # mLayerVolFracWat - Volumetric fraction of total water in each layer
+  ########################################################################
+  mLayerVolFracWat <- ncvar_get(summaout, 'mLayerVolFracWat')[hruIndex,] #Soil Moisture
+  mLayerVolFracWat_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerVolFracWat[x])) #Make a list of data
+  mLayerVolFracWat_soil <- setNames(mLayerVolFracWat_soil,  #Set the names of different soil layers automatically
+                                    lapply(seq(1,max(nSoil)), function(x) paste0('mLayerVolFracWat_SoilLayer',x)))
+
+  ########################################################################
+  # mLayerVolFracIce - Volumetric fraction of Ice in each layer
+  ########################################################################
+  mLayerVolFracIce <- ncvar_get(summaout, 'mLayerVolFracIce')[hruIndex,] #Soil Moisture
+  mLayerVolFracIce_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerVolFracIce[x])) #Make a list of data
+  mLayerVolFracIce_soil <- setNames(mLayerVolFracIce_soil,  #Set the names of different soil layers automatically
+                                    lapply(seq(1,max(nSoil)), function(x) paste0('mLayerVolFracIce_SoilLayer',x)))
+
+  ########################################################################
+  # mLayerMatricHead - Matric Head in each layer
+  ########################################################################
+  mLayerMatricHead <- ncvar_get(summaout, 'mLayerMatricHead')[hruIndex,] #Soil Moisture
+  mLayerMatricHead_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerMatricHead[x])) #Make a list of data
+  mLayerMatricHead_soil <- setNames(mLayerMatricHead_soil,  #Set the names of different soil layers automatically
+                                    lapply(seq(1,max(nSoil)), function(x) paste0('mLayerMatricHead_SoilLayer',x)))
+
+  ########################################################################
+  # mLayerTemp - #Temperature in each layer
+  ########################################################################
+  mLayerTemp <- ncvar_get(summaout, 'mLayerTemp')[hruIndex,]-272.15 #Soil Moisture
+  mLayerTemp_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerTemp[x])) #Make a list of data
+  mLayerTemp_soil <- setNames(mLayerTemp_soil,  #Set the names of different soil layers automatically
+                                    lapply(seq(1,max(nSoil)), function(x) paste0('mLayerTemp_SoilLayer',x)))
+
+  ########################################################################
+  # mLayerHeight - #Height in each layer
+  ########################################################################
+  # mLayerHeight <- ncvar_get(summaout, 'mLayerHeight') #Soil Moisture
+  # mLayerHeight_soil <- data.frame(sapply(soillayerindexlist, function(x) mLayerHeight[x])) #Make a list of data
+  # mLayerHeight_soil <- setNames(mLayerHeight_soil,  #Set the names of different soil layers automatically
+  #                             lapply(seq(1,max(nSoil)), function(x) paste0('mLayerHeight_SoilLayer',x)))
+
+
+  ########################################################################
+  # Create dataframes for plotting
+  ########################################################################
+  #sumdata_long <- melt(sumdata, id='stimevec')
+  sumdata_all <- data.frame(stime, pptrate, airtemp, windspd, scalarRainfall, SWRadAtm, LWRadAtm, #forcings
+                            scalarSurfaceTemp, #others
+                            scalarCanopyIce, scalarCanopyLiq, scalarSnowDepth, #Canopy
+                            scalarBelowCanopySolar, scalarSenHeatGround, scalarSenHeatTotal,
+                            scalarLatHeatGround, scalarLatHeatTotal, scalarSnowSublimation, scalarThroughfallSnow, scalarThroughfallRain,
+                            scalarRainPlusMelt , scalarInfiltration, scalarExfiltration,  #Soil
+                            scalarSWE, scalarSurfaceRunoff, scalarSoilDrainage,
+                            mLayerVolFracLiq_soil, #Soil layer variables
+                            mLayerVolFracWat_soil,
+                            mLayerVolFracIce_soil,
+                            mLayerTemp_soil,
+                            check.names=FALSE)
+  # return(sumdata_all)
+
+  sumdata_units_lookup <- data.frame(var_names=c("stime", "pptrate", "airtemp", "windspd", "scalarRainfall", "SWRadAtm", "LWRadAtm", #forcings
+                                                 "fieldCapacity", "HRUarea", "scalarSurfaceTemp", #others
+                                                 "scalarCanopyIce", "scalarCanopyLiq", "scalarSnowDepth", #Canopy
+                                                 "scalarBelowCanopySolar", "scalarWindspdCanopyBottom", "scalarSenHeatGround", "scalarSenHeatTotal",
+                                                 "scalarLatHeatGround", "scalarLatHeatTotal", "scalarSnowSublimation", "scalarThroughfallSnow", "scalarThroughfallRain",
+                                                 "scalarRainPlusMelt" , "scalarInfiltration", "scalarExfiltration",  #Soil
+                                                 "scalarSWE", "scalarSurfaceRunoff", "scalarSoilDrainage",
+                                                 "mLayerTemp_SoilLayer1", "mLayerTemp_SoilLayer2", "mLayerTemp_SoilLayer3", "mLayerTemp_SoilLayer4", "mLayerTemp_SoilLayer5","mLayerTemp_SoilLayer6","mLayerTemp_SoilLayer7","mLayerTemp_SoilLayer8",
+                                                 "mLayerHeight_SoilLayer1","mLayerHeight_SoilLayer2", "mLayerHeight_SoilLayer3", "mLayerHeight_SoilLayer4", "mLayerHeight_SoilLayer5", "mLayerHeight_SoilLayer6", "mLayerHeight_SoilLayer7", "mLayerHeight_SoilLayer8"),
+                                         y_lab=c("h", "mm h-1", "°C", "m s-1", "mm h-1", "W m-2", "W m-2", #forcings
+                                                 " ", " ", "°C", #others
+                                                 "mm", "mm", "mm", #canopy
+                                                 "W m-2", "m s-1",  "W m-2",  "W m-2",
+                                                 "W m-2",  "W m-2", "mm h-1", "mm h-1", "mm h-1",
+                                                 "mm s-1", "mm s-1", "mm s-1", #Soil
+                                                 "kg m-2", "mm h-1", "mm h-1",
+                                                 "°C", "°C", "°C", "°C", "°C", "°C", "°C", "°C",
+                                                 "m", "m", "m", "m", "m", "m", "m", "m" ))
+
+  #######################################################################
+  #Individual plots
+  #######################################################################
+  plot_list = list()
+  for (yvar in names(sumdata_all)[-1]) {
+    p = ggplot(sumdata_all, aes_string(x = stime, y = yvar)) +
+      geom_line()+
+      scale_x_datetime(date_labels = "%b %y")+
+      # ylab(units_list[i])+
+      ylab(sumdata_units_lookup$y_lab[match(yvar, sumdata_units_lookup$var_names)])+ #Matching units with variable using a loopup table
+      ggtitle(yvar)+
+      theme_bw()+
+
+
+      theme(plot.title = element_text(hjust = 0.5),
+            axis.title.x=element_blank())
+    plot_list[[yvar]] = p
+  }
+
+  # create pdf where each page is a separate plot. ,
+  pdf(outfile,  width=7, height=1.5)
+  for (yvar in names(sumdata_all)[-1]) {
+    print(plot_list[yvar])
+  }
+  dev.off()
+}
+
+#
+# temporalplot('data_raw/benchmarking/PNW_3L_3H.nc', 1, 'figures/summaoutput_3l_3H.pdf')
+# temporalplot('data_raw/benchmarking/PNW_8L_1H.nc', 1, 'figures/summaoutput_8l_1H.pdf')
+# temporalplot('data_raw/benchmarking/PNW_8L_3H.nc', 1, 'figures/summaoutput_8l_3H.pdf')
+
